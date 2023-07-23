cbd16f65225abe1ed85586049b827d07d5ceb3ee
WalsonK <71435596+WalsonK@users.noreply.github.com>
2023-07-23 20:09:04 +0200

    ✨ Camera preview + capture base64image

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { CameraPreview, CameraPreviewOptions, CameraPreviewPictureOptions } from '@capacitor-community/camera-preview';
+
+import '@capacitor-community/camera-preview'
+
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  image = null;
+  cameraActive = false;
 
   constructor() {}
 
+  openCamera(){
+    const cameraPreviewOptions: CameraPreviewOptions = {
+      position: 'front',
+      parent: 'cameraPreview',
+      className: 'cameraPreview'
+    };
+    CameraPreview.start(cameraPreviewOptions);
+    this.cameraActive = true;
+  }
+
+  async stopCamera(){
+    await CameraPreview.stop();
+    this.cameraActive = false;
+  }
+
+  async captureImage(){
+    const CameraPreviewPictureOptions: CameraPreviewPictureOptions = {
+      quality: 85
+    };
+
+    const result = await CameraPreview.capture(CameraPreviewPictureOptions);
+    const base64Picture = result.value;
+    console.log(base64Picture)
+    this.stopCamera();
+  }
+
+  flipCamera(){
+    CameraPreview.flip();
+  }
 }
