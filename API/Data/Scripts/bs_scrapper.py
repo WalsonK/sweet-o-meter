2833e74b8f6a34f32fc450e8e5f98e950a8f0126
WalsonK <71435596+WalsonK@users.noreply.github.com>
2023-07-30 11:32:29 +0200

    🚑️ bs Scrapper : scrap istock

--- API/Data/Scripts/bs_scrapper.py
+++ API/Data/Scripts/bs_scrapper.py
@@ -8,26 +8,32 @@ headers = {
                   'Chrome/58.0.3029.110 Safari/537.3'
 }
 
-
 images_links = []
-for i in range(351):
-    link = f"https://www.shutterstock.com/search/candy-apples?image_type=photo&channel=nooffset&page={i + 1}"
+for i in range(1):
+    link = f"https://www.istockphoto.com/fr/search/2/image?mediatype=photography&phrase=barbe%20%C3%A0%20papa" \
+          f"&alloweduse=off&page={i + 1}"
     res = requests.get(link, headers=headers)
-    print(f"Lien {i} status : {res.status_code}")
     if res.status_code == 200:
-        images = re.findall(r'<img .*?src="(https://www\.shutterstock\.com/.*?)"', res.text)
-        print(f"Sur la page {i}, il y a {len(images)} images")
+        images = re.findall(r'<img .*?src="(https://media\.istockphoto\.com.*?)"', res.text)
+        print(f"Sur la page {i} il y a {len(images)} images")
         images_links += images
 
 print(f"--------------------------------------------\n total image {len(images_links)}")
-print(images_links)
+
+
+def retire_ampersand(url):
+    return url.replace("&amp;", "&")
+
+
+images_links = [retire_ampersand(url) for url in images_links]
 
 for i, image_l in enumerate(images_links):
     try:
         response = requests.get(image_l)
         response.raise_for_status()
-        with open(os.path.join("../Datasets/Originals/Apple_candy/shutterstock", f"image_{i + 311}.jpg"), "wb") as f:
+        with open(os.path.join("../Datasets/Originals/Cotton_candy/istock", f"image_{i + 310}.jpg"), "wb") as f:
             f.write(response.content)
-        print(f"Téléchargement : {i + 1} / {len(images_links)}")
+        if (i + 1) % 100 == 0:
+            print(f"Téléchargement : {i + 1} / {len(images_links)}")
     except requests.exceptions.RequestException as e:
         print(f"Echec lors du téléchargement de l'image {i + 1}: {e}")
